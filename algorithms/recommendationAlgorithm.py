75c4e0520f97a295475f2c54dae7fd53123d6d07
Chad <crakoczy@umich.edu>
2023-03-09 22:20:13 -0500

    3/9 work session

--- algorithms/recommendationAlgorithm.py
+++ algorithms/recommendationAlgorithm.py
@@ -1,5 +1,3 @@
-NUM_OF_OUTPUTS = 5
-
 # Hard coded dataset, to be replaced by Google Maps API
 DATASET = [
     ('Tomokun Noodle Bar', 'Japanese', 2),
@@ -28,7 +26,50 @@ DATASET = [
     ("The Chop House", 'Steak', 4)
 ]
 
-def recommendationAlgorithm(cuisine, lowPrice, highPrice, weights):
+NUM_OF_OUTPUTS = len(DATASET)
+
+class Recommender():
+
+    def __init__(self, users):
+        self.users = users
+        self.submitted_preferences_ = 0
+        self.weighed_preferences_ = []
+
+    def addUserPreferences(self, cuisine, lowPrice, highPrice, weights=[0.5, 0.5]):
+        
+        prefs = recommendationAlgorithm(cuisine, lowPrice, highPrice, weights)
+
+        if self.weighed_preferences_ == []:
+            self.weighed_preferences_ = prefs
+        else:
+            for i in range(len(self.weighed_preferences_)):
+                for j in prefs:
+                    if self.weighed_preferences_[i][0] == j[0]:
+                        self.weighed_preferences_[i][1] += j[1]
+                        break
+        
+        submitted_preferences_ += 1
+        if submitted_preferences_ == users:
+            return getPreferences()
+        else:
+            return -1
+
+    def getPreferences(self):
+
+        # Argsort the list of scores
+        sorted_list = [i for i in sorted(self.weighed_preferences_, key=lambda x:x[1])]
+        sorted_list.reverse()
+
+        # Return the top restaurant names and scores
+        output = sorted_list[:5]
+
+        return output
+
+    def getSubmittedPreferences(self):
+        return self.submitted_preferences_
+
+
+def recommendationAlgorithm(cuisine, lowPrice, highPrice, weights=[0.5, 0.5]):
     scores = []
 
     for restaurant in DATASET:
@@ -38,8 +79,8 @@ def recommendationAlgorithm(cuisine, lowPrice, highPrice, weights):
             priceScore = (- abs(restaurant[2] - lowPrice) - abs(restaurant[2] - highPrice)) / 2
         
         cuisineScore = 0
-        if cuisine == restaurant[1]:
-            cuisineScore = 3
+        if restaurant[1] in cuisine:
+            loc = 3 - cuisine.index(restaurant[1])
         
         scores.append(weights[0] * priceScore + weights[1] * cuisineScore)
 
@@ -47,7 +88,7 @@ def recommendationAlgorithm(cuisine, lowPrice, highPrice, weights):
     sorted_list = [i[0] for i in sorted(enumerate(scores), key=lambda x:x[1])]
     sorted_list.reverse()
 
-    # Return the top NUM_OF_OUTPUTS restaurant names and scores
+    # Return the top restaurant names and scores
     output = [(DATASET[sorted_list[i]][0], scores[sorted_list[i]]) for i in range(NUM_OF_OUTPUTS)]
     
     return output
@@ -55,8 +96,8 @@ def recommendationAlgorithm(cuisine, lowPrice, highPrice, weights):
 # TESTING
 
 # if __name__ == '__main__':
-#     print(recommendationAlgorithm('Japanese', 2, 3, [0.5, 0.5]))
-#     print(recommendationAlgorithm('Steak', 1, 2, [0.1, 0.9]))
-#     print(recommendationAlgorithm('Steak', 1, 2, [0.9, 0.1]))
-#     print(recommendationAlgorithm('Italian', 3, 3, [0, 1]))
+#     print(recommendationAlgorithm(['Japanese', 'Korean', 'Chinese'], 2, 3))
+#     print(recommendationAlgorithm(['Steak', 'Korean', 'Italian'], 1, 2, [0.1, 0.9]))
+#     print(recommendationAlgorithm(['Steak', 'Korean', 'Italian'], 1, 2, [0.9, 0.1]))
+#     print(recommendationAlgorithm(['Steak', 'Chinese', 'French'], 3, 3, [0, 1]))
 
--- app.py
+++ app.py
@@ -4,17 +4,61 @@ import uuid
 import hashlib
 import flask
 import arrow
-from flask import Flask, session, redirect, request, render_template
+import random
+from flask import Flask, session, redirect, request, render_template, url_for
+
+#from algorithms.recommendationAlgorithm import recommendationAlgorithm
 
 app = Flask(__name__)
 
-@app.route('/grouphome/')
-def change_password():
-    return flask.render_template("grouphome.html")
+sessions = {}
+current_session_id = 0
 
-@app.route('/groups/', methods=['POST'])
-def logout_account():
-    if flask.request.form['operation'] == 'Create Group':
-        return flask.render_template("creategroup.html")
-    else:
-        return flask.render_template("joingroup.html")
+@app.route('/login/', methods = ['POST'])
+def login_user():
+    pass
+
+def index_page():
+    return render_template('index.html')
+
+@app.route('/')
+def index_page():
+    return render_template('index.html')
+
+@app.route('/group/')
+def group_page():
+    session_code = request.args.get('session_code', None)
+    group = request.args.getlist('group')
+    return render_template('group.html', session_code=session_code, group=group)
+
+@app.route('/grouphome/', methods = ['GET', 'POST'])
+def group_home():
+    global current_session_id
+    if request.method == 'POST':
+        operation = request.form.get('operation')
+
+        if operation == "create":
+            session_code = str(current_session_id)
+            current_session_id = current_session_id + 1
+
+            sessions[session_code] = ["Chad"]
+        else:
+            session_code = request.form.get('session_join')
+            
+            sessions[session_code].append("Avi")
+
+        group = sessions[session_code]
+
+        return redirect(url_for('group_page', session_code=session_code, group=group))
+    return render_template('grouphome.html')
+
+# @app.route('/groups/')
+# def logout_account():
+#     if flask.request.form['operation'] == 'Create Group':
+#         return flask.render_template("creategroup.html")
+#     else:
+#         return flask.render_template("joingroup.html")
+
+@app.route('/preference')
+def preference_page():
+    return render_template('preference.html')
